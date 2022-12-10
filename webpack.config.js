ad4283afc2a51fbec319b6d1ebfc76a14b83721c
svv <vvsh1978@mail.ru>
2022-12-10 14:31:29 +0500

    feat: create webpack.config.js and .browserslistrc

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+const mode = process.env.NODE_ENV || 'development';
+const devMode = mode === 'development';
+const target = devMode ? 'web' : 'browserslist';
+const devtool = devMode ? 'source-map' : undefined;
+
+module.exports = {
+  mode,
+  target,
+  devtool,
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    filename: 'main.js',
+    clean: true,
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
