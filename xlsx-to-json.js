6b8eb18028f2c3d863f6d1f57ffc8f138ee0a615
Victor <vvsh1978@mail.ru>
2022-12-25 20:14:04 +0500

    feat: modify xlsx-to-json.js and ng_db.xlsx

--- xlsx-to-json.js
+++ xlsx-to-json.js
@@ -1,2 +1,20 @@
-const xlsx = require('xlsx');
+const xlsx = require('xlsx')
+const workbook = xlsx.readFile('./src/db/ng_db.xlsx')
+const sheet_name_list = workbook.SheetNames // получаем список листов
+
+sheet_name_list.forEach(list => {
+    const worksheet = workbook.Sheets[list]
+
+    let headers = {}
+    let data = []
+
+    for (z in worksheet) {
+        if (z[0] === '!') continue
+        let col = z.substring(0, 1)
+        let row = z.substring(1)
+        console.log(col, row);
+    }
+})
+
+
 
