2dae5ae4c73240fc6cc9981f95792f42f94943b7
svv <vvsh1978@mail.ru>
2023-04-05 00:36:03 +0500

    feat: edit selectObjectInList.js change class selecting object

--- src/module/selectObjectInList.js
+++ src/module/selectObjectInList.js
@@ -2,18 +2,27 @@ import { getData } from "./getData"
 
 export const selectObjectInList = () => {
   const objectList = document.getElementById('object-list')
+  
+
 
   const selectObject = (e) => {
     if (e.target.closest('.object-list-item')) {
-      getData().then(data => {
-        data.forEach(item => {
-          if (item.name === e.target.textContent) {
-            console.log(item);
-          }
-        })
+      const list = document.querySelectorAll('.object-list-item')
+      list.forEach(item => {
+        item.classList.remove('object-list-item_active')
       })
+      e.target.classList.add('object-list-item_active')
     }
-    }
+  }
+
+
+  // getData().then(data => {
+  //   data.forEach(item => {
+  //     if (item.name === e.target.textContent) {
+  //       console.log(item);
+  //     }
+  //   })
+  // })
 
 
   objectList.addEventListener('click', selectObject)
