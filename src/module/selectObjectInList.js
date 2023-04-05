721f178eda8607b30d0dd37590be5ebdcbb19ded
svv <vvsh1978@mail.ru>
2023-04-05 16:54:48 +0500

    feat: add selectObject and getId function in selectObjectInList.js

--- src/module/selectObjectInList.js
+++ src/module/selectObjectInList.js
@@ -2,28 +2,46 @@ import { getData } from "./getData"
 
 export const selectObjectInList = () => {
   const objectList = document.getElementById('object-list')
-  
 
+  let idObject
 
   const selectObject = (e) => {
-    if (e.target.closest('.object-list-item')) {
-      const list = document.querySelectorAll('.object-list-item')
-      list.forEach(item => {
-        item.classList.remove('object-list-item_active')
+    const list = document.querySelectorAll('.object-list-item')
+
+    list.forEach(item => {
+      item.classList.remove('object-list-item_active')
+    })
+    e.target.classList.add('object-list-item_active')
+  }
+
+  const listMap = document.querySelectorAll('.object')
+  
+  const getId = (e) => {
+    getData().then(data => {
+      data.forEach(item => {
+        if (item.name === e.target.textContent) {
+          return item.id
+        }
       })
-      e.target.classList.add('object-list-item_active')
+      
+    })
+  }
+
+    // getData().then(data => {
+    //   data.forEach(item => {
+    //     if (item.name === e.target.textContent) {
+    //       idObject = item.id
+    //     }
+    //   })
+    // })
+  
+
+  const getObject = (e) => {
+    if (e.target.closest('.object-list-item')) {
+      selectObject(e)
+      getId(e)
     }
   }
 
-
-  // getData().then(data => {
-  //   data.forEach(item => {
-  //     if (item.name === e.target.textContent) {
-  //       console.log(item);
-  //     }
-  //   })
-  // })
-
-
-  objectList.addEventListener('click', selectObject)
+  objectList.addEventListener('click', getObject)
 }
